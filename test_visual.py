bf2e7b2a55400954d32891883002565d19554949
97Simei <lsmdlut@gmail.com>
2022-05-31 11:16:54 +0200

    change test_visual.py

--- test_visual.py
+++ test_visual.py
@@ -1,2 +1,43 @@
-def test_demo():
-    assert 100 == 100
+from os import path
+import sys
+from visualFunctionDetail import PlotFunction
+
+input_path = "./data/test.csv"
+output_path=''
+
+def test_plot_missing_value_function():
+    plot_worker = PlotFunction(
+        input_path=input_path,
+        title="test_sample",
+        function_type='missing_value',
+        plot_type='',
+        target_columns='',
+        output_path=output_path
+    )
+    plot_worker.plot_missing_value()
+    assert path.exists(output_path + "_missing_value.html") is True
+def test_plot_distribution_function():
+    plot_worker = PlotFunction(
+        input_path=input_path,
+        title="test_sample",
+        function_type='missing_value',
+        plot_type='',
+        target_columns='',
+        output_path=output_path
+    )
+    plot_worker.plot_distribution()
+    assert path.exists(output_path + "distribution.png") is True
+def test_plot_correlation_function():
+    plot_worker = PlotFunction(
+        input_path=input_path,
+        title="test_sample",
+        function_type='missing_value',
+        plot_type='',
+        target_columns='',
+        output_path=output_path
+    )
+    plot_worker.plot_correlation()
+    assert path.exists(output_path + "heatmap.png") is True
+
+
+
